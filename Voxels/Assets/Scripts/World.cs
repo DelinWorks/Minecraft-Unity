6105b405acce3650508658fc2f88c0ec0558360f
TURKY-M7MD <45469625+TURKY-M7MD@users.noreply.github.com>
2019-12-22 16:59:11 +0300

    Update

--- Voxels/Assets/Scripts/World.cs
+++ Voxels/Assets/Scripts/World.cs
@@ -125,8 +125,7 @@ public class World : MonoBehaviour
         GL.End();
         GL.Begin(GL.LINE_STRIP);
         GLMat.SetPass(0);
-        GL.Color(Color.red);
-        GL.Color(new Color(0, 1, 0, .03f));
+        GL.Color(new Color(0, 1, 0, .05f));
         foreach (var vertex in PlayerController.mesh.vertices)
         {
             GL.Vertex(new Vector3(vertex.x, 0, vertex.z));
